ca3146546c062fa64cd0e1921bbc5a04a0da15b1
TravelCatAlex <alex@yourcatbackpack.com>
2026-08-26 13:16:08 -0400

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,6 @@
 'use client';
-import SpinDJDashboard from '../components/SpinDJDashboard';
-export default function DashboardPage() {
+import SpinDJDashboard from '@/app/components/SpinDJDashboard';
+
+export default function Home() {
   return <SpinDJDashboard />;
 }
